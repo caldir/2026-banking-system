a79b66391cd602f84328a26bef10bb46187db737
kestrel-turbo <daddaasdad218@gmail.com>
2026-05-18 21:31:22 +0300

    fix 4 task 4

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ from domain.enums import AccountStatus, Currency
 from exceptions.banking_exceptions import AccountFrozenError
 from domain.accounts import AbstractAccount, BankAccount
 
-print('Banking system started')
-print(AccountStatus.ACTIVE.value)
-print(Currency.RUB.value)
-print(AccountFrozenError)
+from exceptions.banking_exceptions import (
+    InsufficientFundsError,
+    InvalidOperationError,
+)
 
-print(AbstractAccount)
+print('Banking system started')
 
 account = BankAccount(owner_id="client_1")
+print(account)
+account.deposit(1000)
+account.withdraw(300)
+print(account.balance)
 print(account.get_account_info())
+try:
+    account.withdraw(100000)
+except InsufficientFundsError as error:
+    print('InsufficientFundError cauth:', error)
+try:
+    account.deposit(-100)
+except InvalidOperationError as error:
+    print("InvalidOperationError caught:", error)
