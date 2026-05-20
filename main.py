b75f7ee1015b8096c52efadb3c6efd3a1f8b3ae6
kestrel-turbo <daddaasdad218@gmail.com>
2026-05-20 15:34:02 +0300

    21 task

--- main.py
+++ main.py
@@ -17,19 +17,12 @@ from services.bank import Bank
 
 print('Banking system started')
 
-from domain.clients import Client
-from services.bank import Bank
-from exceptions.banking_exceptions import InvalidOperationError
-
 bank = Bank("Mentor Bank")
-
 client = Client(full_name="Ivan Petrov", age=30)
+account = BankAccount(owner_id=client.client_id, balance=1000)
 
 bank.add_client(client)
+bank.open_account(client.client_id, account)
 
-print(bank.clients)
-
-try:
-    bank.add_client(client)
-except InvalidOperationError as error:
-    print("Duplicate client caught:", error)
+print(bank.accounts)
+print(client.accounts)
--- services/bank.py
+++ services/bank.py
@@ -20,6 +20,14 @@ class Bank():
 
     def add_client(self, client):
         if client.client_id in self.clients:
-            raise InvalidOperationError('Client already exist')
+            raise InvalidOperationError('Client already exists')
         self.clients[client.client_id] = client
+
+    def open_account(self,client,account):
+        if client.client_id not in self.clients:
+            raise InvalidOperationError('Client not found')
+        if account.account_id in self.accounts:
+            raise InvalidOperationError('Account already exists')
+        self.accounts[account_id] = account
+        self.clients[client_id].add_account(account.account_id)
         
